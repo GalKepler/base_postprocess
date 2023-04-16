c9cce46f16e0157319da33c128efae714e079cda
GalKepler <galkepler@gmail.com>
2023-04-16 12:59:15 +0300

    added a basic file-getter method for the 'Session' class

--- base_tracts/entities/session/session.py
+++ base_tracts/entities/session/session.py
@@ -13,15 +13,43 @@ class Session:
 
     DATE_FORMAT = "%Y%m%d%H%M"  #: The date format used for session IDs.
     PREFIX = "ses-"
+    ENTITIES_FORMATS = {
+        "direction": "dir-{direction}",
+        "space": "space-{space}",
+        "resolution": "res-{resolution}",
+        "label": "label-{label}",
+        "atlas": "atlas-{atlas}",
+        "description": "desc-{description}",
+        "suffix": "{suffix}",
+        "extension": ".{extension}",
+    }
 
     def __init__(self, path: Union[str, Path]):
         """
         Initialize a Session object.
 
         Args:
-            path: Path to the session directory.
+            path (Union[str, Path]): The path to the session directory.
         """
         self.path = Path(path)
+
+    def __repr__(self) -> str:
+        """
+        The session ID.
+
+        Returns:
+            The session ID.
+        """
+        return f"Session ({self.path.name})"
+
+    def __str__(self) -> str:
+        """
+        The session ID.
+
+        Returns:
+            The session ID.
+        """
+        return self.id
 
     def parse_id(self) -> str:
         """
@@ -41,23 +69,31 @@ class Session:
         """
         return datetime.datetime.strptime(self.parse_id(), self.DATE_FORMAT)
 
-    def __repr__(self) -> str:
+    def get(self, entities: dict, return_associated: bool = False) -> list[Path]:
         """
-        The session ID.
+        Get the path(s) to the file(s) that match the given entities.
+
+        Args:
+            entities (dict): The entities to match.
+            return_associated (bool, optional): Whether to return associated files.
+            Defaults to False.
+
+        Raises:
+            ValueError: If the datatype entity is not provided.
 
         Returns:
-            The session ID.
+            list[Path]: The paths to the file(s) that match the given entities.
         """
-        return f"Session ({self.path.name})"
-
-    def __str__(self) -> str:
-        """
-        The session ID.
-
-        Returns:
-            The session ID.
-        """
-        return self.id
+        datatype = entities.pop("datatype", None)
+        if not datatype:
+            raise ValueError("The datatype entity is required.")
+        regex = "*"
+        for entity, value in self.ENTITIES_FORMATS.items():
+            if entity in entities:
+                regex += f"_{value.format(**entities)}"
+            elif entity == "extension":
+                regex += ".nii*" if not return_associated else ".*"
+        return list(self.path.glob(f"{datatype}/{regex}"))
 
     @property
     def id(self) -> str:
